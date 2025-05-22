6883e8aef8c6cca6fa3d2c9d43c2a1acd4130997
20039888aB <151379335+20039888aB@users.noreply.github.com>
2025-05-22 20:36:00 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -17,7 +17,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
-const analytics = getAnalytics(app);
+
 
 setPersistence(auth, browserLocalPersistence)
   .then(() => {
